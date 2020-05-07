4eeadb7c2b2278750e146cd493b581cb7d962246
Kanishk Gupta <kanishkgupta1234@gmail.com>
2020-05-07 10:25:35 +0530

    Fixed redux middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -11,8 +11,7 @@ const reducers=combineReducers({
     UI:uiReducer
 })
 
-const store=createStore(reducers,initialState,compose(applyMiddleware(...middleware),
-window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store=createStore(reducers,initialState,compose(applyMiddleware(...middleware)
 )
 );
 export default store;
